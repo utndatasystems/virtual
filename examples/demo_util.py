6ebacc11e9ef0a348a946973dcd1333bf2f227dc
Mihail Stoian <stoianmmihail@yahoo.com>
2025-03-26 15:11:08 +0100

    Fixed https parquet size

--- examples/demo_util.py
+++ examples/demo_util.py
@@ -2,12 +2,17 @@ import requests
 import os
 
 def get_file_size(parquet_filepath):
-  if parquet_filepath.startswith('s3://'):
-    url = parquet_filepath.replace('s3://', 'https://s3.amazonaws.com/')
+  def size_impl(url):
     response = requests.head(url)
     if response.status_code == 200:
       return int(response.headers.get('Content-Length', 0))
     else:
       raise Exception(f'Error: {response.status_code} - {response.reason}')
+
+  if parquet_filepath.startswith('s3://'):
+    url = parquet_filepath.replace('s3://', 'https://s3.amazonaws.com/')
+    return size_impl(url)
+  elif parquet_filepath.startswith('https://'):
+    return size_impl(parquet_filepath)
   else:
     return os.path.getsize(parquet_filepath)
